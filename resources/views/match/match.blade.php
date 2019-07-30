d132a97c52ee2a400f87fedb6068b893374a7912
yasaruniversitesi <>
2019-07-30 11:19:30 +0300

    match butonu artık seçilen option id'sini tutabiliyor.

--- resources/views/match/match.blade.php
+++ resources/views/match/match.blade.php
@@ -19,10 +19,10 @@
     <h2>Ürüne Stok Sahibi Ata</h2>
     <p>Ürün ve Stok Sahibi Seçiniz</p>
     <br>
-    <form action="/action_page.php">
+    <form>
         <div class="form-group">
             <label for="sel1">Ürün (Seçiniz)</label>
-            <select name="productSelector"  class="form-control" id="productselector" >
+            <select name="productSelector"  class="form-control" id="productSelector" >
                 <option value="" selected disabled hidden>Ürün ismi seçiniz</option>
                 @foreach($products as $item)
                     <option value="{{ $item->id }}">{{ $item->name }}</option>
@@ -34,36 +34,56 @@
 
         <div class="form-group">
             <label for="sel1">Stok Sahibi (Seçiniz)</label>
-            <select name="ownerSelector" class="form-control" id="ownerselector" onselect="function f('$item2->id') {
+            <select name="ownerSelector" class="form-control" id="ownerSelector">
 
-                    }">
+
                 <option value="" selected disabled hidden>Kullanıcı ismi seçiniz</option>
                 @foreach($owners as $item2)
                     <option value="{{ $item2->id }}">{{ $item2->name }}</option>
                 @endforeach
             </select>
+
+
         </div>
         <br>
-        <a class="btn btn-primary btn-block" role="button" href="deneme{{"/".$item2->id}}{{"/".$item->id}}" >Atama Yap</a>
+        <button class="btn btn-primary btn-block" id="matchbutton"></button>
+        <button type="button" class="btn btn-primary btn-block">Full-Width Button</button>
+
+        <script>
+            $("#ownerSelector").change(function () {
+                var den = ($(this).val());
+               alert(den)
+
+
+            });
+
+            document.getElementById("ownerSelector").onchange = function() {
+
+                document.getElementById("matchbutton").href = "http://ordernow/"+this.value+"/";
+
+            }
+        </script>
+
+
+
+
+
+
+
 
     </form>
 
-
-
 </div>
 
-{{----------------------------------------------------------}}
-{{----------------------------------------------------------}}
-{{----------------------------------------------------------}}
+{{--
+
+<a class="btn btn-primary btn-block" role="button" id="matchbutton" >Atama Yap</a>
+href="deneme{{"/".$item->id}}{{"/".$item2->id}}"
 
 
 
-{{----------------------------------------------------------}}
-{{----------------------------------------------------------}}
-{{----------------------------------------------------------}}
 
-
-
+--}}
 
 
 
