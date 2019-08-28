cf2928963a5fe69fc045282f10de6580d56cc6df
yasaruniversitesi <>
2019-08-28 15:37:38 +0300

    tanıtım sayfası kartlarına animasyon ve sayfa öğelerine gölge eklendi

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -11,6 +11,7 @@ $agent = new Agent();
 
 
 <head>
+
     <title>Staj.Site</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -21,6 +22,8 @@ $agent = new Agent();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Maven+Pro&display=swap" rel="stylesheet">
+
 </head>
 
 <nav style="padding: 25px" class="navbar navbar-expand-md bg-dark navbar-dark" >
@@ -177,22 +180,22 @@ $agent = new Agent();
 
 
     <div class="jumbotron">
-        <h1>Staj.site Tanıtım Sayfası</h1>
-        <p>Ziyaret etmek istediğiniz sayfanın tanıtım kartındaki git butonuna tıklayabilirsiniz.</p>
+        <h1 style="font-style: italic">Staj.site Tanıtım Sayfası</h1>
+        <p style="font-style: italic">Ziyaret etmek istediğiniz sayfanın tanıtım kartındaki git butonuna tıklayabilirsiniz.</p>
     </div>
 
     <div class="card-deck ">
         <div class="card ">
             <div class="card-body">
-            <h5 class="card-title">Ürünleri Görüntüle</h5>
-            <img class="card-img-top" src="https://i.hizliresim.com/yGj4Vy.jpg"  >
+                <h5 class="card-title">Ürünleri Görüntüle</h5>
+                <img class="card-img-top" src="https://i.hizliresim.com/yGj4Vy.jpg"  >
 
 
                 <p class="card-text">Bu bölümde kayıt edilen ürünler listelenir ve seçili ürün stok ekleme/çıkarma ve ürünü silme işlemleri
-                yapılabilir. Bu işlemler için kullanıcı girişi gereklidir. </p>
+                    yapılabilir. Bu işlemler için kullanıcı girişi gereklidir. </p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><button class=" btn btn-outline-success btn-block" onclick="window.location.href='/products' ">Git</button></small>
+                <small class="text-muted"><button class=" btn btn-outline-secondary btn-block" onclick="window.location.href='/products' ">Git</button></small>
             </div>
         </div>
         <div class="card">
@@ -202,7 +205,7 @@ $agent = new Agent();
                 <p class="card-text">Bu bölümde kayıt edilen kullanıcılar listelenir. Kullanıcı ismi ve kullanıcının profil tanımını buradan öğrenebilirsiniz.</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><button class=" btn btn-outline-success btn-block">Git</button></small>
+                <small class="text-muted"><button class=" btn btn-outline-secondary btn-block">Git</button></small>
             </div>
         </div>
         <div class="card">
@@ -212,25 +215,25 @@ $agent = new Agent();
                 <p class="card-text">Bu bölümde kayıt edilen kullanıcılar listelenir. Kullanıcı ismi ve kullanıcının profil tanımını buradan öğrenebilirsiniz.</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><button class=" btn btn-outline-success btn-block">Git</button></small>
+                <small class="text-muted"><button class=" btn btn-outline-secondary btn-block">Git</button></small>
             </div>
         </div>
 
 
-            </div>
+    </div>
 
     <br>
     <div class="card-deck">
         <div class="card">
             <div class="card-body">
-            <h5 class="card-title">Ürün Ekle</h5>
-            <img class="card-img-top" src="https://i.hizliresim.com/QPyMLy.jpg" alt="Card image cap">
+                <h5 class="card-title">Ürün Ekle</h5>
+                <img class="card-img-top" src="https://i.hizliresim.com/QPyMLy.jpg" alt="Card image cap">
 
 
                 <p class="card-text">Bu sayfada ürün kaydı için bir form sayfası gösterilir.</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><button class=" btn btn-outline-success btn-block">Git</button></small>
+                <small class="text-muted"><button class=" btn btn-outline-secondary btn-block">Git</button></small>
             </div>
         </div>
         <div class="card">
@@ -242,7 +245,7 @@ $agent = new Agent();
                 <p class="card-text">By sayfada kullanıcı kaydı için bir form sayfası gösterilir.</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><button class=" btn btn-outline-success btn-block">Git</button></small>
+                <small class="text-muted"><button class=" btn btn-outline-secondary btn-block">Git</button></small>
             </div>
         </div>
         <div class="card">
@@ -254,7 +257,7 @@ $agent = new Agent();
                 <p class="card-text">Bu sayfada kullanıcı ve stok eşleştirmesi için bir form sayfası gösterilir.</p>
             </div>
             <div class="card-footer">
-                <small class="text-muted"><button class=" btn btn-outline-success btn-block">Git</button></small>
+                <small class="text-muted"><button class=" btn btn-outline-secondary btn-block">Git</button></small>
             </div>
         </div>
 
@@ -327,10 +330,83 @@ $agent = new Agent();
         padding: 25px;
     }
 
-    
-    jumbotron{
-        background-color: red;
+
+    .jumbotron{
+        background-color: white;
+        box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.22);
     }
+    .cards-list {
+        z-index: 0;
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
+
+
+
+    .card .card_image {
+        width: inherit;
+        height: inherit;
+        border-radius: 40px;
+    }
+
+    .card .card_image img {
+        width: inherit;
+        height: inherit;
+        border-radius: 40px;
+        object-fit: cover;
+    }
+
+    .card .card_title {
+        text-align: center;
+        border-radius: 0px 0px 40px 40px;
+        font-family: sans-serif;
+        font-weight: bold;
+        font-size: 30px;
+        margin-top: -80px;
+        height: 40px;
+    }
+
+    .card:hover {
+        transform: scale(0.9, 0.9);
+        box-shadow: 5px 5px 30px 15px rgba(0,0,0,0.25),
+        -5px -5px 30px 15px rgba(0,0,0,0.22);
+    }
+
+
+    .card {
+
+        box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.22);
+        cursor: pointer;
+        transition: 0.7s;
+    }
+
+    @media all and (max-width: 500px) {
+        .card-list {
+            /* On small screens, we are no longer using row direction but column */
+            flex-direction: column;
+        }
+    }
+ h5{
+    font-style: italic;
+}
+
+    /*
+     .card {
+     margin: 30px auto;
+     width: 300px;
+     height: 300px;
+     border-radius: 40px;
+     background-image: url('https://i.redd.it/b3esnz5ra34y.jpg');
+     background-size: cover;
+     background-repeat: no-repeat;
+     background-position: center;
+     background-repeat: no-repeat;
+     box-shadow: 5px 5px 30px 7px rgba(0,0,0,0.25), -5px -5px 30px 7px rgba(0,0,0,0.22);
+     transition: 0.4s;
+     }
+     */
 </style>
 
 
